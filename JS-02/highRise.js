85ac33c60a6a1b081589447bb800d65328751d9c
vashatasha <110487016+vashatasha@users.noreply.github.com>
2023-12-26 17:48:17 +0100

    fixed bag with number of floor

--- JS-02/highRise.js
+++ JS-02/highRise.js
@@ -1,8 +1,8 @@
 let allFloors = prompt("What is the number of floors in a house?");
 let amountFlats = prompt("How many flats are on the floor?");
-let flatNumber = prompt("What is the number of the flat that you looking for?");
+let flatNumber = prompt("What is the number of the flat that you looking for?")-1;
 
-let entrance = Math.ceil (flatNumber / (allFloors * amountFlats));
+let entrance = Math.floor (flatNumber / (allFloors * amountFlats))+1;
 let residual = flatNumber % (allFloors * amountFlats);
-let floor =  Math.ceil(residual / amountFlats);
+let floor =  Math.floor (residual / amountFlats)+1;
 alert (`The flat is in ${entrance} entrance on ${floor} floor!`);
